e03a59270b4052c4737e5b54ac9c441e2114a46f
jvvalente <joaovalente2010@icloud.com>
2021-04-08 12:04:48 -0400

    Initial check-in of module HW4_API

---
+++ HW4_API/Controllers/InventoryController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HW4_API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+
+namespace HW4_API.Controllers
+{
+
+    [ApiController]
+    [Route("Controller")]
+    public class InventoryController : ControllerBase
+    {
+
+        [HttpGet("test")]
+        public string Test()
+        {
+            return "Hello, World!";
+        }
+
+        [HttpGet("GetAll")]
+        public ActionResult<List<Product>> Get()
+        {
+            return Ok(DataContext.Inventory);
+        }
+
+    }
+}
---
+++ HW4_API/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using HW4_API.Models;
+
+namespace HW4_API
+{
+    public class DataContext
+    {
+            public static List<Product> Inventory = new List<Product>
+            {
+                new ProductByQuantity(2.00, 20, "Ketchup", "Canned & Packaged Foods", 0002),
+                new ProductByQuantity(2.00, 35, "Mayonnaise", "Canned & Packaged Foods", 0003),
+                new ProductByQuantity(5.00, 25, "Chocolate bar", "Canned & Packaged Foods", 0004),
+                new ProductByQuantity(3.50, 100, "Paper Towels", "Miscellaneous Kitchen Items", 0005),
+                new ProductByQuantity(2.35, 80, "Plastic Wrap", "Miscellaneous Kitchen Items", 0006),
+                new ProductByQuantity(1.50, 90, "Yougurt", "Refrigerated Foods", 0007),
+                new ProductByQuantity(1.25, 150, "Bagels", "Bakery", 0008),
+                new ProductByQuantity(1.00, 200, "Bread", "Bakery", 0009),
+                new ProductByQuantity(3.50, 45, "Cereal", "Breakfast", 0010),
+        };
+    }
+}
